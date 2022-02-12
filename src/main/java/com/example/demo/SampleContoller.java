8f9f168902b71f00a839c47f743035690b021d0d
Yogesh More <37771780+moreyog@users.noreply.github.com>
2022-02-12 13:04:23 +0530

    Update SampleContoller.java
    test on master

--- src/main/java/com/example/demo/SampleContoller.java
+++ src/main/java/com/example/demo/SampleContoller.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SampleContoller {
 
+    //New comment added
     @GetMapping
     public String hello(){
         return "Hello World!!";
